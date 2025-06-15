d9fa1ae2eadf4c6eb0b91f80ddb017e937b0ff63
deanna-stanley <dstanley.git@gmail.com>
2025-06-15 14:05:19 -0700

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+
+const url = 'mongodb://127.0.0.1:27017/nucampsite';
+const connect = mongoose.connect(url, {});
+
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
+);
 
 const app = express();
 
