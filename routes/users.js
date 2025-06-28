07b7674f069d26dcdef9e7f0452209a174dac758
deanna-stanley <dstanley.git@gmail.com>
2025-06-27 21:28:20 -0700

    CORS

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ const router = express.Router();
 //     res.send('respond with a resource');
 // });
 router.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     User.find()
     .then(users => {
         res.statusCode = 200;
@@ -20,7 +21,7 @@ router.route('/')
     .catch(err => next(err));
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions, (req, res) => {
     const user = new User({ username: req.body.username });
     
     User.register(user, req.body.password)
@@ -47,14 +48,14 @@ router.post('/signup', (req, res) => {
         });
 });
 
-router.post('/login', passport.authenticate('local', { session: false}), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local', { session: false}), (req, res) => {
     const token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res, next) => {  
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {  
     if (req.session && req.session.user) {  // Check for a specific user property
         req.session.destroy((err) => {
             if (err) {
